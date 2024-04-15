2d915397b55107c12bb8bdf7bce999961cc8f336
cwyspy100 <cwyspy100@163.com>
2024-04-15 17:23:13 +0800

    use pipenv

---
+++ index/GetIndexData.py
@@ -0,0 +1,11 @@
+
+import akshare as ak
+
+# stock_zh_index_spot_em_df = ak.stock_zh_index_spot_em(symbol="上证系列指数")
+# print(stock_zh_index_spot_em_df.tail())
+
+
+
+stock_zh_index_daily_df = ak.stock_zh_index_daily(symbol="sh000300")
+print(stock_zh_index_daily_df)
+
