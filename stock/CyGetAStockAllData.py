19a214aef9bce62b04f81a756bbd4ce0ee0e3e60
cwyspy100 <cwyspy100@163.com>
2024-06-03 07:20:09 +0800

    get a stock hold

--- stock/CyGetAStockAllData.py
+++ stock/CyGetAStockAllData.py
@@ -6,24 +6,25 @@ import os
 import time
 
 
-def get_stock_data_by_name(symbol, start_date='20230410', end_date='20240410'):
-    file_name = f"D:\\abu\\stock\\{symbol}_{start_date}_{end_date}"
+def get_stock_data_by_name(symbol, start_date='20230410', end_date='20240531'):
+    file_name = f"~/abu/cn/stock/{symbol}_{start_date}_{end_date}"
     column_names = {'日期': 'date', '开盘': 'open', '收盘': 'close', '最高': 'high', '最低': 'low', '成交量': 'volume'}
 
     if os.path.exists(file_name):
         stock_data = pd.read_csv(file_name)
         # stock_data.rename(columns=column_names, inplace=True)
-        # print('get data from file')
+        print('get data from file')
     else:
-        return pd.DataFrame()
-        # try:
-        #     stock_data = ak.stock_zh_a_hist(symbol, period='daily', start_date=start_date, end_date=end_date, adjust="")
-        #     stock_data = stock_data.rename(columns=column_names)
-        #     selected_columns = stock_data.filter(items=column_names.values())
-        #     selected_columns.to_csv(file_name, index=False)
-        # except Exception as e:
-        #     print(f"获取数据失败，错误信息：{e}")
-        #     return pd.DataFrame()
+        # return pd.DataFrame()
+        try:
+            print('get data from file{}'.format(symbol))
+            stock_data = ak.stock_zh_a_hist(symbol, period='daily', start_date=start_date, end_date=end_date, adjust="")
+            stock_data = stock_data.rename(columns=column_names)
+            selected_columns = stock_data.filter(items=column_names.values())
+            selected_columns.to_csv(file_name, index=False)
+        except Exception as e:
+            print(f"获取数据失败，错误信息：{e}")
+            return pd.DataFrame()
 
     return stock_data
 
@@ -136,7 +137,8 @@ if __name__ == '__main__':
     进行选股,主要测试通过价格和成交量选股
     """
     start_time = time.time()
-    pick_stock(end_date='20240415')
-    # get_all_stock_data()
+    # pick_stock(end_date='20240415')
+    get_all_stock_data()
+    # get_stock_data_by_name("600519")
     end_time = time.time()
     print(f"耗时：{end_time - start_time}")
