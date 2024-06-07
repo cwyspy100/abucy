a48a8f663e317ab466a81e2f8f7659f9e920d230
cwyspy100 <cwyspy100@163.com>
2024-06-07 10:51:56 +0800

    实时更新当天最新数据处理

--- stock/CyUpdateAStockData.py
+++ stock/CyUpdateAStockData.py
@@ -11,20 +11,27 @@ from util import RegUtil, PathUtil
 
 # 1. 获取股票的实时行情
 def get_all_latest_stock():
+    """
+    只用于获取当天实时的行情
+    :return:
+    """
     current_date = date.today()
     current_date = current_date.strftime('%Y%m%d')
     file_name = f"abu/cn/all/{current_date}.csv"
     file_path = PathUtil.get_user_path(file_name)
-    if os.path.exists(file_path):
-        stock_data = pd.read_csv(file_path)
-        # stock_data.rename(columns=column_names, inplace=True)
-        print('get data from file')
-    else:
-        stock_data = ak.stock_zh_a_spot_em()
-        stock_data.to_csv(file_path, index=False)
+    # if os.path.exists(file_path):
+    #     stock_data = pd.read_csv(file_path)
+    #     # stock_data.rename(columns=column_names, inplace=True)
+    #     print('get data from file')
+    # else:
+    stock_data = ak.stock_zh_a_spot_em()
+    stock_data.to_csv(file_path, index=False)
     return stock_data
 
 
+"""
+获取历史行情
+"""
 def pick_stock_by_date(current_date):
     global stock_data
     file_name = f"abu/cn/all/{current_date}.csv"
@@ -59,14 +66,20 @@ def update_all_stock_data_simple(start_date='20240410', end_date='20240412', all
     使用最新的实时数据来更新历史数据，只更新一天
     start_date :end_date 是股票单个数据的时间
     all_stock_file_date 是全部最新数据文件的时间
+    当更新当天数据的时候end_date 和all_stock_file_date 相同
 
     """
-    stock_latest_data = pick_stock_by_date(all_stock_file_date)
+    stock_latest_data = None
+    if end_date == all_stock_file_date:
+        stock_latest_data = get_all_latest_stock()
+    else:
+        stock_latest_data = pick_stock_by_date(all_stock_file_date)
     if stock_latest_data is None:
         return
 
     # 将20240410 变为 2024-04-10
     save_current_date = f"{all_stock_file_date[:4]}-{all_stock_file_date[4:6]}-{all_stock_file_date[6:]}"
+    end_date_str = f"{end_date[:4]}-{end_date[4:6]}-{end_date[6:]}"
     for code in stock_latest_data['代码']:
         # 开头是8的股票代码，不处理
         # code = f"{j:06}"
@@ -77,24 +90,31 @@ def update_all_stock_data_simple(start_date='20240410', end_date='20240412', all
         if stock_data is None or stock_data.size == 0:
             continue
 
-        # 使用concat函数将新数据添加到现有的DataFrame中
         stock_new_data = stock_latest_data[stock_latest_data['代码'] == code]
         new_data = pd.DataFrame({'date': [save_current_date], 'open': [stock_new_data['今开'].iloc[0]]
                                     , 'close': [stock_new_data['最新价'].iloc[0]],
                                  'high': [stock_new_data['最高'].iloc[0]]
                                     , 'low': [stock_new_data['最低'].iloc[0]],
                                  'volume': [stock_new_data['成交量'].iloc[0]]})
-
-        stock_data = pd.concat([stock_data, new_data], ignore_index=True)
+        if end_date_str not in stock_data['date'].values:
+            # 使用concat函数将新数据添加到现有的DataFrame中
+            stock_data = pd.concat([stock_data, new_data], ignore_index=True)
+        else:
+            stock_data.loc[stock_data['date'] == end_date_str, 'open'] = new_data['open'].iloc[0]
+            stock_data.loc[stock_data['date'] == end_date_str, 'close'] = new_data['close'].iloc[0]
+            stock_data.loc[stock_data['date'] == end_date_str, 'high'] = new_data['high'].iloc[0]
+            stock_data.loc[stock_data['date'] == end_date_str, 'low'] = new_data['low'].iloc[0]
+            stock_data.loc[stock_data['date'] == end_date_str, 'volume'] = new_data['volume'].iloc[0]
 
         file_name = f"~/abu/cn/stock/{code}_{start_date}_{all_stock_file_date}"
         stock_data.to_csv(file_name, index=False)
 
         # # 删除文件，不删除文件可以进行每天测试
-        file_name = f"abu/cn/stock/{code}_{start_date}_{end_date}"
-        file_path = PathUtil.get_user_path(file_name)
-        if os.path.exists(file_path):
-            os.remove(file_path)
+        if all_stock_file_date != end_date:
+            file_name = f"abu/cn/stock/{code}_{start_date}_{end_date}"
+            file_path = PathUtil.get_user_path(file_name)
+            if os.path.exists(file_path):
+                os.remove(file_path)
 
 
 def pick_stock(start_date='20230410', end_date='20240410'):
@@ -240,17 +260,15 @@ todo list
 """
 if __name__ == '__main__':
     start = time.time()
-    current_date = 20240604
+    current_date = 20240607
     # # 周一减少3天
-    check_date = current_date - 1
-    # check_date = 20240531
-    # # # 1、获取股票的实时行情
-    get_all_latest_stock()
-    # # # 2、将每个股票的实时行情保存到历史数据，更新多天有问题,只更新一天，周一需要单独设置两个时间
-    update_all_stock_data_simple("20230410", str(check_date), str(current_date))
-    # 3、对数据进行选股
+    check_date = current_date
+
+    # # 1、将每个股票的实时行情保存到历史数据，更新多天有问题,只更新一天，周一需要单独设置两个时间
+    # update_all_stock_data_simple("20230410", str(check_date), str(current_date))
+    # # 2、对数据进行选股
     pick_stock(end_date=str(current_date))
-    # 4、监控昨天选股情况
+    # # 3、监控昨天选股情况
     check_choose_stock_change(current_date)
 
     # 4、回测股票
